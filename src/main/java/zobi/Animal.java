309270e001a7c19a672bcb3e3333138b20f92cec
Maic <fulldisclosure@hotmail.fr>
2015-05-21 16:51:45 +0200

    Animal.java + AnimalTest.java + animal.xhtml

---
+++ src/main/java/zobi/Animal.java
@@ -0,0 +1,99 @@
+package zobi;
+
+import javax.enterprise.inject.Model;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+@Model
+public class Animal {
+
+	private String matricule;
+	private int age;
+	private String birthPlace;
+	private String species;
+
+	/**
+	 * @param context a FacesContext.
+	 * @param component a UIComponent.
+	 * @param value an Object the birthPlace to validate.
+	 * @throws ValidatorException
+	 */
+	public void validateBirthPlace(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		String birthPlace = (String) value;
+		String errorMessage = null;
+		if (birthPlace.length() < 4) {
+			errorMessage = "The birth place must be at least 4 characters long.";
+		}
+		if (errorMessage == null && !birthPlace.startsWith("ZOO_") && !birthPlace.matches("WILD")) {
+			errorMessage = "The birth place must be on format 'WILD' or 'ZOO_something'.";
+		}
+		if (errorMessage != null) {
+			FacesMessage facesMessage = new FacesMessage(errorMessage);
+			facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(facesMessage);
+		}
+	}
+
+	/**
+	 * @return the matricule
+	 */
+	public String getMatricule() {
+		return matricule;
+	}
+
+	/**
+	 * @param matricule
+	 *            the matricule to set
+	 */
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the birthPlace
+	 */
+	public String getBirthPlace() {
+		return birthPlace;
+	}
+
+	/**
+	 * @param birthPlace
+	 *            the birthPlace to set
+	 */
+	public void setBirthPlace(String birthPlace) {
+		this.birthPlace = birthPlace;
+	}
+
+	/**
+	 * @return the species
+	 */
+	public String getSpecies() {
+		return species;
+	}
+
+	/**
+	 * @param species
+	 *            the species to set
+	 */
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+}
