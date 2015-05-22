9dc5bcc77a7242f45c0b60cea64fe4f39dc398b7
Maic <fulldisclosure@hotmail.fr>
2015-05-22 09:31:03 +0200

    AllTests.java for JUnit

---
+++ src/test/java/zobi/AllTests.java
@@ -0,0 +1,11 @@
+package zobi;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AnimalTest.class, TestLogin.class })
+public class AllTests {
+
+}
