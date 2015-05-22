d1785d8a8ce8e6c6c0a4245902ab28e90ffc94b8
Maic <fulldisclosure@hotmail.fr>
2015-05-22 11:58:04 +0200

    SpeciesTest.java + EnclosureTest.java

--- src/test/java/zobi/AllTests.java
+++ src/test/java/zobi/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AnimalTest.class, LoginTest.class, UserTest.class })
+@SuiteClasses({ AnimalTest.class, LoginTest.class, UserTest.class, EnclosureTest.class, SpeciesTest.class })
 public class AllTests {
 
 }
---
+++ src/test/java/zobi/SpeciesTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package zobi;
+
+import org.junit.Test;
+
+/**
+ * @author humanbooster
+ *
+ */
+public class SpeciesTest {
+
+	/**
+	 * Test method for {@link zobi.Species#getCommonName()}.
+	 */
+	@Test
+	public final void testGetCommonName() {
+		System.out.println("Not yet implemented"); // TODO
+	}
+
+}
